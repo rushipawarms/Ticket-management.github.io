f100d12fa671d6119018e19f5f81b78b7fe7dfd1
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2021-10-25 00:28:11 +0530

    add

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ removebtn.addEventListener("click",function(){
 modalC.addEventListener("keydown",(e)=>{
    
     let key=e.key;
-    if(key=="Enter")
+    if(key=="+")
     {
         
         createtask(modalcolor, textcont.value);
